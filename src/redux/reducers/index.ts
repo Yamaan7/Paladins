9b6b52312c68dfbabad97ac287e2cc8500f21fca
Yamaan7 <yamaanmuhammad@gmail.com>
2025-04-22 15:50:15 +0500

    removed unnecessary files

--- src/redux/reducers/index.ts
+++
@@ -1,4 +0,0 @@
-export * from "./appSlice";
-export { default as app } from "./appSlice";
-export * from "./userSlice";
-export { default as user } from "./userSlice";
--- src/types/SwapSelect.d.ts
+++
@@ -1,4 +0,0 @@
-export interface SwapTokenType {
-  name: string;
-  icon: string;
-}
