af59978bc343a3d4aaa4e0ace1ad78d820c1fc6b
Yamaan7 <yamaanmuhammad@gmail.com>
2025-04-22 16:11:19 +0500

    removed more unnecessary files

--- src/main.tsx
+++ src/main.tsx
@@ -3,14 +3,11 @@ import "@/styles/index.css";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
-import { TokenProvider } from "./providers/TokenProvider.tsx";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <AppProvider>
-      <TokenProvider>
-        <App />
-      </TokenProvider>
+      <App />
     </AppProvider>
   </React.StrictMode>
 );
--- src/providers/AppProvider.tsx
+++ src/providers/AppProvider.tsx
@@ -2,7 +2,6 @@ import { queryClient } from "@/utils";
 import { QueryClientProvider } from "@tanstack/react-query";
 import React from "react";
 import ThemeProvider from "./ThemeProvider";
-import { Web3ModalProvider } from "./Web3ModalProvider";
 import { Provider } from "react-redux";
 import { store } from "@/redux/store";
 
@@ -14,11 +13,9 @@ const AppProvider = ({ children }: Props) => {
   return (
     <ThemeProvider>
       <Provider store={store}>
-        <Web3ModalProvider>
-          <QueryClientProvider client={queryClient}>
-            {children}
-          </QueryClientProvider>
-        </Web3ModalProvider>
+        <QueryClientProvider client={queryClient}>
+          {children}
+        </QueryClientProvider>
       </Provider>
     </ThemeProvider>
   );
